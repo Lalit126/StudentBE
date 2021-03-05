da81bcf540b23c24a4b2eca7319c5293f6d6f751
lalit126 <55103714+Lalit126@users.noreply.github.com>
2021-03-04 22:29:49 -0500

    Some changes in paths!

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-const data = require('./Student_Data.json')
-const {v4:uuid4} = require('uuid');
+const data = require('./Student_Data.json');
+const { v4: uuidv4 } = require('uuid');
 var cors = require('cors')
+var path = require('path');
+
 app.use(cors())
 
+let  uuidData = data.map(i=>{
+  return {
+      ...i, 
+      Id: uuidv4()
+  }
+ })
+ 
 
-app.get('/', (req, res) => {
-  res.send(data)
+
+app.get('/allrecords', (req, res) => {
+   res.send(uuidData)
 })
 
+app.get('/recorddetails', (req, res) => {
+  console.log(req.query,'req.query')  
+  let filter= uuidData.filter(i=>i.Id === req.query.Id)
+  console.log(filter,'filter');
+   res.send(filter)
+})
 
-// app.get('/allrecords', (req, res)=> {
+// app.get('/recorddetails/:id', (req, res) => {
+//   console.log(req.params,'req.params')  //mention
 
-//   let uuidData = data.map(i=>{
-
-//     return {
-
-//       ...Id ,
-//       Id : uuid4()
-//     }
-//   })
-
-//   res.send(uuidData)
 // })
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/send.html'));
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
